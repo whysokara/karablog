8adc4841a0e963d6c993888a626dadbc30bd60f4
Kara <kara_himanshu@yahoo.com>
2021-12-29 13:16:08 +0530

    File rename
    App.js to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 // import Home from "./pages/home/Home";
 import Topbar from "./components/topbar/Topbar";
+import Settings from "./pages/settings/Settings";
 // import Single from "./pages/single/Single";
-import Write from "./pages/write/Write";
+// import Write from "./pages/write/Write";
 
 function App() {
   return (
     <div className="App">
       <Topbar />
-      <Write />
+      <Settings />
     </div>
   );
 }
