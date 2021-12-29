8d1fca70d364c0b980d8b933e01dd0c0a688ed35
Kara <kara_himanshu@yahoo.com>
2021-12-29 15:11:34 +0530

    Basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,40 @@
-// import Home from "./pages/home/Home";
+import Home from "./pages/home/Home";
 import Topbar from "./components/topbar/Topbar";
-// import Login from "./pages/login/Login";
+import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
-// import Settings from "./pages/settings/Settings";
-// import Single from "./pages/single/Single";
-// import Write from "./pages/write/Write";
+import Settings from "./pages/settings/Settings";
+import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
+
+//router
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import SinglePost from "./components/singlePost/SinglePost";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Topbar />
-      <Register />
-    </div>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/write">
+          <Write />
+        </Route>
+        <Route path="/settings">
+          <Settings />
+        </Route>
+        <Route path="/post/:postId">
+          <Single />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
