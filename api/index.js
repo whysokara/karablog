0e6536ecccbc038e22d376c0a50730219d614fdc
Kara <kara_himanshu@yahoo.com>
2021-12-29 17:08:41 +0530

    MongoDb Setup

--- api/index.js
+++ api/index.js
@@ -1,7 +1,24 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useCreateIndex: true,
+  })
+  .then(console.log("MongoDb Connected"))
+  .catch((err) => console.log(err));
+
+app.use("/", (req, res) => {
+  console.log("This is main url");
+});
 
 const PORT = "5000";
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on ports ${PORT}`);
 });
