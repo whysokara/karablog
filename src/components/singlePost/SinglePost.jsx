2e99089f747f3e61712acd10a0aa33896c764633
Kara <kara_himanshu@yahoo.com>
2021-12-29 12:21:27 +0530

    SinglePost Added

--- src/components/singlePost/SinglePost.jsx
+++ src/components/singlePost/SinglePost.jsx
@@ -13,6 +13,26 @@ const SinglePost = () => {
             <i className="singlePostIcon far fa-trash-alt"></i>
           </div>
         </h1>
+        <div className="singlePostInfo">
+          <span className="singlePostAuthor">
+            Author: <b>kara</b>
+          </span>
+          <span className="singlePostDate">1 hour ago</span>
+        </div>
+        <p className="singlePostDesc">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam
+          libero vitae dolore veritatis earum. Alias quod non iste ex amet
+          minus, itaque dignissimos accusamus maxime quasi corrupti quaerat,
+          officiis beatae, atque aspernatur dolorum incidunt. Dicta labore
+          voluptas ea cumque amet nam autem voluptatem sit consequatur id enim
+          et totam earum, dolores accusantium ipsum quisquam suscipit. Quaerat
+          ratione inventore, saepe excepturi expedita architecto, incidunt
+          asperiores ducimus unde temporibus, animi minima! Animi dicta, et
+          minima nostrum debitis laboriosam maxime voluptatem, corporis maiores
+          optio officia. Explicabo consequatur nihil quam eveniet repellendus
+          ipsam provident ea voluptate. Provident ad iusto atque autem minima
+          debitis velit?
+        </p>
       </div>
     </div>
   );
