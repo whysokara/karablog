331b7650299ad79654034eca3edde1cff4762cfa
Kara <kara_himanshu@yahoo.com>
2021-12-29 14:39:00 +0530

    Register Added
    Started

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 // import Home from "./pages/home/Home";
 import Topbar from "./components/topbar/Topbar";
-import Settings from "./pages/settings/Settings";
+// import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+// import Settings from "./pages/settings/Settings";
 // import Single from "./pages/single/Single";
 // import Write from "./pages/write/Write";
 
@@ -8,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <Topbar />
-      <Settings />
+      <Register />
     </div>
   );
 }
---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,27 @@
+import "./register.css";
+import Sidebar from "../../components/sidebar/Sidebar";
+const Register = () => {
+  return (
+    <div className="register">
+      <span className="registerTitle">Register</span>
+      <form action="" className="registerForm">
+        <label htmlFor="">email</label>
+        <input
+          className="registerInput"
+          type="text"
+          placeholder="Enter your email.."
+        />{" "}
+        <label htmlFor="">Password</label>
+        <input
+          className="registerInput"
+          type="password"
+          placeholder="Enter your password"
+        />
+        <button className="registerButton">Register</button>
+      </form>
+      <button className="registerLoginButton">Login</button>
+    </div>
+  );
+};
+
+export default Register;
